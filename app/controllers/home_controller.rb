b1ade6d5199d0755de05c195044e0a622a7b4f1e
michael.zech <michael.zech@hotmail.de>
2021-11-06 15:31:28 +0100

    Delete all completed items

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,9 @@ class HomeController < ApplicationController
         List.all.destroy_all
         redirect_to lists_url, notice: "All items have become deleted."
     end
+
+    def delete_completed
+        List.where({completed: true}).destroy_all
+        redirect_to lists_url, notice: "All completed items have become deleted."
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "lists#index"
   resources :lists
-  get '/home/delete_list', to: "home#delete_list"
+  delete '/home/delete_list', to: "home#delete_list"
+  delete '/home/delete_completed', to: "home#delete_completed", as: "del_compl"
 end
